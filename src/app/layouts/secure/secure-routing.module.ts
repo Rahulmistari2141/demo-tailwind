4ebd98a08bb6a71561edd9a7d63c704ea58c13d6
rahulmistari2141 <rahulmistari2141@gmail.com>
2024-08-08 18:57:44 +0530

    header page - design with made responsive & banner section design with made responsive

--- src/app/layouts/secure/secure-routing.module.ts
+++ src/app/layouts/secure/secure-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { SecureComponent } from './secure.component';
 
 const routes: Routes = [
-  { path: '', component: SecureComponent },
-  { path: 'dashboard', loadChildren: () => import('../../module/secure/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'master', loadChildren: () => import('../../module/secure/master/master.module').then(m => m.MasterModule) },
+  // { path: '', component: SecureComponent },
+  { path: 'secure/dashboard', loadChildren: () => import('../../module/secure/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'secure/master', loadChildren: () => import('../../module/secure/master/master.module').then(m => m.MasterModule) },
 ];
 
 @NgModule({
--- src/app/layouts/secure/secure.module.ts
+++ src/app/layouts/secure/secure.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SecureRoutingModule } from './secure-routing.module';
 import { SecureComponent } from './secure.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
